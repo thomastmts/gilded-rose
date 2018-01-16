49f58be878055cec2dbe7df4b924dbbc97fefc38
Floriane44 <fpeltier@et.esiea.fr>
2018-01-16 14:00:05 +0100

    Création du projet

---
+++ projet/src/main/java/fr/esiea/archi/App.java
@@ -0,0 +1,13 @@
+package fr.esiea.archi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
