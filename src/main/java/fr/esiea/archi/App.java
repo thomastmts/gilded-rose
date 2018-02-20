65e7d9f2327645fd4ec425a40c235910ea52ac8a
Floriane44 <fpeltier@et.esiea.fr>
2018-02-20 16:40:14 +0100

    Amélioration de GildedRose

--- src/main/java/fr/esiea/archi/App.java
+++ src/main/java/fr/esiea/archi/App.java
@@ -1,5 +1,7 @@
 package fr.esiea.archi;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,61 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //createItem();
     }
+    
+    /*public static void createItem(){
+    	System.out.println("Créer un item");
+    	System.out.println("Choisissez un nom : ");
+    	
+    	System.out.println("1. Backstage passes to a TAFKAL80ETC concert");
+    	System.out.println("2. Sulfuras, Hand of Ragnaros");
+    	System.out.println("3. Aged Brie");
+    	System.out.println("4. Confiture");
+    	
+    	Scanner keyboard = new Scanner(System.in);
+    	boolean error = false;
+    	int numname;
+    	int sellin;
+    	int quality;
+    	
+    	do{
+    		System.out.println("Entrez un des chiffres de la liste : ");
+        	numname = keyboard.nextInt();
+        	if(numname <= 0 || numname > 4) error = true;
+        	
+        	System.out.println("Donnez le nombre de jours restant pour vendre l'article : ");
+        	sellin = keyboard.nextInt();
+        	
+        	System.out.println("Donnez un nombre pour définir la qualité de l'article (entre 0 et 50) : ");
+        	quality = keyboard.nextInt();
+    	} while(error == true);
+    	
+    	String name = new String();
+    	
+    	switch (numname) {
+		case 1:
+			name = "Backstage passes to a TAFKAL80ETC concert";
+			break;
+			
+		case 2:
+			name = "Sulfuras, Hand of Ragnaros";
+			break;
+			
+		case 3:
+			name = "Aged Brie";
+			break;
+			
+		case 4:
+			name = "Confiture";
+			break;
+
+		default:
+			break;
+		}
+    	
+    	Item item = new Item(name, sellin, quality);
+    	
+    	keyboard.close();
+    }*/
 }
--- src/main/java/fr/esiea/archi/GildedRose.java
+++ src/main/java/fr/esiea/archi/GildedRose.java
@@ -9,54 +9,67 @@ class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
+            
+        	if(!items[i].name.equals("Sulfuras, Hand of Ragnaros")){
+        		
+        		items[i].sellIn--;
+        		
+        		// Si l'item est Aged Brie
+        		if (items[i].name.equals("Aged Brie")) updateBrie(i);
+        			
+            	// Si l'item est Backstage Passes
+        		else if(items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) updatePasses(i);
+        			
+        		// Si l'item n'est ni Aged Brie ni Backstage Passes (ni Sulfuras)
+        		else updateOthers(i);
+        	}
+        	
         }
     }
+
+	public void updateBrie(int i){
+    	// Si sa qualité est inférieur à la qualité maximale
+		if (items[i].quality < 50) {
+            
+			items[i].quality++;
+            
+            // Après la date de péremption de l'item, on augmente de nouveau sa qualité : elle aura augmenté de 2 au final
+            if (items[i].sellIn < 0) items[i].quality++;
+  
+		}
+    }
+    
+	private void updatePasses(int i) {
+    	// Si sa qualité est inférieur à la qualité maximale
+		if (items[i].quality < 50){
+			
+			items[i].quality++;
+			
+			// S'il reste 10 jours ou moins avant le concert, on augmente sa qualité : elle aura augmenté de 2 au final
+        	if (items[i].sellIn < 11) {
+        		items[i].quality++;
+                    
+        		// S'il reste 6 jours ou moins avant le concert, on augmente de nouveau sa qualité : elle aura augmenté de 3 au final
+        		if (items[i].sellIn < 6) {
+        			items[i].quality++;
+        		}
+            }
+        	
+        	// Sa qualité tombe à 0 après le concert
+        	else if (items[i].sellIn < 0) items[i].quality = 0;
+        	
+		}
+		
+	}
+	
+	private void updateOthers(int i) {
+		if(items[i].quality > 0){
+			items[i].quality--;
+			
+			// Après la date de péremption de l'item, on diminue de nouveau sa qualité : elle aura diminué de 2 au final
+			if(items[i].sellIn < 0) items[i].quality--;
+		}
+		
+	}
+    
 }
