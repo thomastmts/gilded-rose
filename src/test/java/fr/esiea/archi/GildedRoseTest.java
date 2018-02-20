4d8462aa521ec5ae3b3ebda3ad9afb6fc104f72a
Sanyat Mohamed <sanyat.mohamed@gmail.com>
2018-02-20 14:39:52 +0100

    Ajout du premier test sur la qualité de l'item

--- src/test/java/fr/esiea/archi/GildedRoseTest.java
+++ src/test/java/fr/esiea/archi/GildedRoseTest.java
@@ -1,5 +1,16 @@
 package fr.esiea.archi;
 
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
 public class GildedRoseTest {
 
+	@Test
+	public void updateQualityTest() {
+        Item item= new Item("testItem",14,10);
+        Item[] items=new Item[]{item};
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        Assertions.assertThat(item.quality).as("Qualité").isEqualTo(9);
+    }
 }
