c490a8890bec1162f066dcb3df59d2506f619797
Thomas Huang <thomashuang@MacBook-Air-de-Thomas.local>
2018-02-20 15:53:21 +0100

    Ajout test2 backstage

--- src/test/java/fr/esiea/archi/BackstagepassesTest.java
+++ src/test/java/fr/esiea/archi/BackstagepassesTest.java
@@ -16,4 +16,12 @@ public class BackstagepassesTest {
             gildedRose.updateQuality();
             Assertions.assertThat(backItem.quality).as("qualité : ").isEqualTo(12);
         }
+        
+        public void UpdateQualityBackstageAccessINF6() {
+            Item backItem = new Item("Backstage passes to a TAFKAL80ETC concert", 3, 10);
+            Item[] items=new Item[]{backItem};
+            GildedRose gildedRose = new GildedRose(items);
+            gildedRose.updateQuality();
+            Assertions.assertThat(backItem.quality).as("qualité : ").isEqualTo(4);
+        }
 }
