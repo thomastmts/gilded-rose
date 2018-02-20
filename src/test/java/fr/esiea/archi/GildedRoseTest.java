08f7b2572d3f7acdb3729301590e7646017ace74
Sanyat Mohamed <sanyat.mohamed@gmail.com>
2018-02-20 15:37:32 +0100

    Test de la qualité pour Backstage Passes

--- src/test/java/fr/esiea/archi/GildedRoseTest.java
+++ src/test/java/fr/esiea/archi/GildedRoseTest.java
@@ -22,4 +22,13 @@ public class GildedRoseTest {
         gildedRose.updateQuality();
         Assertions.assertThat(agedBrie.quality).as("qualité de Aged Brie ").isEqualTo(11);
     }
+        
+        @Test
+        public void UpdateQualityBackstageAccess() {
+        	Item backItem = new Item("Backstage passes to a TAFKAL80ETC concert", 8, 10);
+            Item[] items=new Item[]{backItem};
+            GildedRose gildedRose = new GildedRose(items);
+            gildedRose.updateQuality();
+            Assertions.assertThat(backItem.quality).as("qualité : ").isEqualTo(12);
+        }
 }
