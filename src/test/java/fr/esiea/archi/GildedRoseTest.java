c2f4a54a2e7e2a97642f023d4c14a7bbc97feaca
Thomas Huang <thomashuang@MacBook-Air-de-Thomas.local>
2018-02-20 15:23:03 +0100

    Ajout test brie

--- src/test/java/fr/esiea/archi/GildedRoseTest.java
+++ src/test/java/fr/esiea/archi/GildedRoseTest.java
@@ -13,4 +13,13 @@ public class GildedRoseTest {
         gildedRose.updateQuality();
         Assertions.assertThat(item.quality).as("Qualité").isEqualTo(9);
     }
+
+        @Test
+        public void UpdateQualityNegativeSellIn10QualityAgedBrie(){
+        Item agedBrie = new Item("Aged Brie", 50, 10);
+        Item[] items=new Item[]{agedBrie};
+        GildedRose gildedRose = new GildedRose(items);
+        gildedRose.updateQuality();
+        Assertions.assertThat(agedBrie.quality).as("qualité de Aged Brie ").isEqualTo(11);
+    }
 }
