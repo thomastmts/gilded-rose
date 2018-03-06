88a8f2d6c4fa776e253209e51d2f25780fed55de
Thomas Huang <thomas_huang47@hotmail.fr>
2018-03-06 18:09:49 +0100

    Ajout du test main

--- src/test/java/fr/esiea/archi/AppTest.java
+++ src/test/java/fr/esiea/archi/AppTest.java
@@ -1,5 +1,5 @@
 package fr.esiea.archi;
-import junit.framework.Test;
+import org.junit.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
@@ -14,24 +14,17 @@ public class AppTest
      *
      * @param testName name of the test case
      */
+    App App;
+    String[] a = {"lol2","lol"};
     public AppTest(String testName)
     {
         super(testName);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite(AppTest.class);
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
+    @Test
     public void testApp()
     {
+        App.main(a);
         assertTrue(true);
     }
 }
